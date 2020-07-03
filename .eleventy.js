3784e57bd8e7b29050a06617d5dcdb27ada190ec
Cam Phan <cam.phan12993@gmail.com>
2020-07-03 09:09:20 +0700

    add config for tailwind

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,9 @@
 module.exports = function (config) {
-  config.addPassthroughCopy('assets');
+  // config.addPassthroughCopy('assets');
   config.addPassthroughCopy('src/admin');
+  config.addPassthroughCopy({ 'src/assets/img': 'assets/img' });
+  config.addPassthroughCopy({ 'src/assets/js': 'assets/js' });
+  config.addPassthroughCopy({ 'src/assets/style.css': 'assets/css/style.css' });
   return {
     dir: {
       input: 'src',
--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,20 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // Specify the paths to all of the template files in your project
+  content: ['./src/**/*.liquid'],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+});
+
+const cssnano = require('cssnano')({
+  preset: 'advanced',
+});
+
 module.exports = {
   plugins: [
     // ...
     require('tailwindcss'),
-    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss, cssnano] : []),
     // ...
   ],
 };
