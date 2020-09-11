f79c37667a0393cf81a334fe7a461314bc58b20f
cam phan <cam.phan12993@gmail.com>
2020-09-12 01:36:26 +0700

    fix postcss config whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -1,21 +1,22 @@
-const purgecss = require('@fullhuman/postcss-purgecss')({
-  // Specify the paths to all of the template files in your project
-  content: ['./src/**/*.liquid'],
-  whitelist: ['active'],
-  // Include any special characters you're using in this regular expression
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+const purgecss = require("@fullhuman/postcss-purgecss")({
+	// Specify the paths to all of the template files in your project
+	content: ["./src/**/*.liquid"],
+	whitelist: ["active"],
+	whitelistPatterns: [/swiper/g],
+	// Include any special characters you're using in this regular expression
+	defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
 });
-const cssnano = require('cssnano')({
-  preset: 'default',
-  discardComments: { removeAll: true },
+const cssnano = require("cssnano")({
+	preset: "default",
+	discardComments: { removeAll: true },
 });
 
 module.exports = {
-  plugins: [
-    // ...
-    require('tailwindcss'),
-    require('autoprefixer'),
-    ...(process.env.NODE_ENV === 'production' ? [purgecss, cssnano] : []),
-    // ...
-  ],
+	plugins: [
+		// ...
+		require("tailwindcss"),
+		require("autoprefixer"),
+		...(process.env.NODE_ENV == "production" ? [purgecss, cssnano] : []),
+		// ...
+	],
 };
