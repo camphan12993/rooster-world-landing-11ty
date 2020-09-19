7ddc2bf1215b6464027289ef088c4c747389c812
cam phan <cam.phan12993@gmail.com>
2020-09-19 21:15:34 +0700

    add fadeout css to whitelist

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
 const purgecss = require("@fullhuman/postcss-purgecss")({
 	// Specify the paths to all of the template files in your project
 	content: ["./src/**/*.liquid"],
-	whitelist: ["active"],
+	whitelist: ["active,fade-out"],
 	whitelistPatterns: [/swiper/g],
 	whitelistPatternsChildren: [/swiper/g],
 	// Include any special characters you're using in this regular expression
