37d267f5f85c360402c34b7defd36a02a80a2734
camphan <camphan@camphans-MacBook-Pro.local>
2020-09-24 11:43:54 +0700

    update slider

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,94 +1,109 @@
 function initSlides() {
-	var mySwiper = new Swiper('.swiper-container', {
-		// Optional parameters
-		loop: true,
-		autoplay: {
-			delay: 15000,
-			disableOnInteraction: false,
-		},
+  var mySwiper = new Swiper(".swiper-container", {
+    effect: "coverflow",
+    fadeEffect: {
+      crossFade: true,
+    },
+    coverflowEffect: {
+      rotate: 30,
+      slideShadows: false,
+    },
+    // Optional parameters
+    loop: true,
+    autoplay: {
+      delay: 15000,
+      disableOnInteraction: false,
+    },
 
-		// Navigation arrows
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-		pagination: {
-			el: '.swiper-pagination',
-		},
-	});
+    // Navigation arrows
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
 }
 
-var mySwiperGallery = new Swiper('.swiper-gallery', {
-	loop: true,
-	// Navigation arrows
-	navigation: {
-		nextEl: '.swiper-button-next',
-		prevEl: '.swiper-button-prev',
-	},
-	pagination: {
-		el: '.swiper-pagination',
-		clickable: true,
-		renderBullet: function (index, className) {
-			return '<span class="' + className + '">' + (index + 1) + '</span>';
-		},
-	},
+var mySwiperGallery = new Swiper(".swiper-gallery", {
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
+  coverflowEffect: {
+    rotate: 30,
+    slideShadows: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + (index + 1) + "</span>";
+    },
+  },
 });
 
 function closeGallery() {
-	document.getElementById('gallery').classList.toggle('hidden');
-	mySwiperGallery.removeAllSlides();
+  document.getElementById("gallery").classList.toggle("hidden");
+  mySwiperGallery.removeAllSlides();
 }
 
 function toggleGallery(imgs) {
-	document.getElementById('gallery').classList.toggle('hidden');
-	for (let index = 0; index < imgs.length; index++) {
-		var slide = `<div class="swiper-slide" style="background-color: rgba(0,0,0,0);background-image: url('${imgs[index]}'); background-size:contain"></div>`;
-		mySwiperGallery.appendSlide(slide);
-	}
+  document.getElementById("gallery").classList.toggle("hidden");
+  for (let index = 0; index < imgs.length; index++) {
+    var slide = `<div class="swiper-slide" style="background-color: rgba(0,0,0,0);background-image: url('${imgs[index]}'); background-size:contain"></div>`;
+    mySwiperGallery.appendSlide(slide);
+  }
+  mySwiperGallery.update();
 }
 
 function toggleMenu() {
-	var mainNav = document.getElementById('nav-menu');
-	mainNav.classList.toggle('active');
-	el = document.createElement('div');
-	el.className = '';
+  var mainNav = document.getElementById("nav-menu");
+  mainNav.classList.toggle("active");
+  el = document.createElement("div");
+  el.className = "";
 }
 
 function toggleSocialMenu() {
-	var menu = document.getElementById('social-menu');
-	menu.classList.toggle('active');
+  var menu = document.getElementById("social-menu");
+  menu.classList.toggle("active");
 }
 
 window.onload = function () {
-	setTimeout(() => {
-		document.getElementById('loading').classList.add('fade-out');
-	}, 2000);
+  setTimeout(() => {
+    document.getElementById("loading").classList.add("fade-out");
+  }, 2000);
 
-	initSlides();
+  initSlides();
 };
 
 window.onresize = function () {
-	var menu = document.getElementById('social-menu');
-	menu.classList.remove('active');
+  var menu = document.getElementById("social-menu");
+  menu.classList.remove("active");
 
-	var mainNav = document.getElementById('nav-menu');
-	mainNav.classList.remove('active');
+  var mainNav = document.getElementById("nav-menu");
+  mainNav.classList.remove("active");
 };
 
 window.onclick = function (e) {
-	var trigger = document.getElementById('social-menu-trigger');
-	var menu = document.getElementById('social-menu');
-	if (trigger != e.target && !trigger.contains(e.target)) {
-		menu.classList.remove('active');
-	}
+  var trigger = document.getElementById("social-menu-trigger");
+  var menu = document.getElementById("social-menu");
+  if (trigger != e.target && !trigger.contains(e.target)) {
+    menu.classList.remove("active");
+  }
 };
 
 if (window.netlifyIdentity) {
-	window.netlifyIdentity.on('init', (user) => {
-		if (!user) {
-			window.netlifyIdentity.on('login', () => {
-				document.location.href = '/admin/';
-			});
-		}
-	});
+  window.netlifyIdentity.on("init", (user) => {
+    if (!user) {
+      window.netlifyIdentity.on("login", () => {
+        document.location.href = "/admin/";
+      });
+    }
+  });
 }
