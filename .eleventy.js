e0ae9b8a2a62aecdbd2a68dcf5f60f80727cecd8
camphan <camphan@camphans-MacBook-Pro.local>
2020-09-24 15:40:27 +0700

    remove unused module

--- .eleventy.js
+++ .eleventy.js
@@ -1,79 +1,69 @@
 const terser = require("terser");
 const htmlmin = require("html-minifier");
 const lazyImagesPlugin = require("eleventy-plugin-lazyimages");
-const svgContents = require("eleventy-plugin-svg-contents");
-const pluginSEO = require("eleventy-plugin-seo-tag");
 
 module.exports = function (config) {
-	// config.addPassthroughCopy('assets');
-	config.addPassthroughCopy("src/admin");
-	config.addPassthroughCopy("src/assets/img");
-	config.addPassthroughCopy({
-		"node_modules/swiper/swiper-bundle.css": "assets/css/swiper-bundle.css",
-	});
-	config.addPassthroughCopy({
-		"node_modules/swiper/swiper-bundle.min.js":
-			"assets/js/swiper-bundle.min.js",
-	});
-	config.addPassthroughCopy("android-chrome-192x192.png");
-	config.addPassthroughCopy("android-chrome-512x512.png");
-	config.addPassthroughCopy("apple-touch-icon.png");
-	config.addPassthroughCopy("favicon-16x16.png");
-	config.addPassthroughCopy("favicon-32x32.png");
-	config.addPassthroughCopy("favicon.ico");
-	config.addPassthroughCopy("site.webmanifest");
-	config.addPlugin(svgContents);
-	config.addPlugin(lazyImagesPlugin, {
-		transformImgPath: (imgPath) => {
-			if (imgPath.startsWith("/assets/img") && !imgPath.startsWith("//")) {
-				return `./src${imgPath}`;
-			}
+  // config.addPassthroughCopy('assets');
+  config.addPassthroughCopy("src/admin");
+  config.addPassthroughCopy("src/assets/img");
+  config.addPassthroughCopy("android-chrome-192x192.png");
+  config.addPassthroughCopy("android-chrome-512x512.png");
+  config.addPassthroughCopy("apple-touch-icon.png");
+  config.addPassthroughCopy("favicon-16x16.png");
+  config.addPassthroughCopy("favicon-32x32.png");
+  config.addPassthroughCopy("favicon.ico");
+  config.addPassthroughCopy("site.webmanifest");
+  config.addPlugin(lazyImagesPlugin, {
+    transformImgPath: (imgPath) => {
+      if (imgPath.startsWith("/assets/img") && !imgPath.startsWith("//")) {
+        return `./src${imgPath}`;
+      }
 
-			return imgPath;
-		},
-	});
+      return imgPath;
+    },
+  });
 
-	config.addLiquidFilter("toJson", function (value) {
-		return JSON.stringify(value);
-	});
+  config.addLiquidFilter("toJson", function (value) {
+    return JSON.stringify(value);
+  });
 
-	// watch
-	config.addWatchTarget("src/assets");
+  // watch
+  config.addWatchTarget("src/assets");
 
-	// minify js
-	config.addFilter("jsmin", function (code) {
-		let minified = terser.minify(code);
-		if (minified.error) {
-			console.log("Terser error: ", minified.error);
-			return code;
-		}
+  // minify js
+  config.addFilter("jsmin", function (code) {
+    let minified = terser.minify(code);
+    if (minified.error) {
+      console.log("Terser error: ", minified.error);
+      return code;
+    }
 
-		return minified.code;
-	});
+    return minified.code;
+  });
 
-	// html minify
-	if (process.env.NODE_ENV == "production") {
-		config.addTransform("htmlmin", function (content, outputPath) {
-			if (outputPath.endsWith(".html")) {
-				let minified = htmlmin.minify(content, {
-					useShortDoctype: true,
-					removeComments: true,
-					collapseWhitespace: true,
-				});
-				return minified;
-			}
+  // html minify
+  if (process.env.NODE_ENV == "production") {
+    config.addTransform("htmlmin", function (content, outputPath) {
+      if (outputPath.endsWith(".html")) {
+        let minified = htmlmin.minify(content, {
+          useShortDoctype: true,
+          removeComments: true,
+          collapseWhitespace: true,
+        });
+        return minified;
+      }
 
-			return content;
-		});
-	}
+      return content;
+    });
+  }
 
-	return {
-		dir: {
-			input: "src",
-		},
-		passthroughFileCopy: true,
-		templateFormats: ["html", "liquid", "md", "11ty.js"],
-		htmlTemplateEngine: "liquid",
-		markdownTemplateEngine: "liquid",
-	};
+  return {
+    dir: {
+      input: "src",
+    },
+    passthroughFileCopy: true,
+    templateFormats: ["html", "liquid", "md", "11ty.js"],
+    htmlTemplateEngine: "liquid",
+    markdownTemplateEngine: "liquid",
+  };
 };
