3c88e4af9bf71c940f60e8e1c568036e81676035
camphan <camphan@camphans-MacBook-Pro.local>
2020-12-01 10:57:52 +0700

    delete netlify admin

--- src/admin/previews/index.js
+++
@@ -1,7 +0,0 @@
-import HeaderPreview from '/admin/previews/header.js';
-import Footer from '/admin/previews/footer.js';
-import About from '/admin/previews/sections/about.js';
-CMS.registerPreviewTemplate('header', HeaderPreview);
-CMS.registerPreviewTemplate('footer', Footer);
-CMS.registerPreviewTemplate('about', About);
-CMS.registerPreviewStyle('/assets/css/style.css');
